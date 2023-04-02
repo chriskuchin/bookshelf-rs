8318b8efaca50309eafa5652195a9d68b3166ca8
Chris Kuchin <github@kchn.dev>
2023-04-01 22:08:42 -0600

    Cors Middleware

--- src/controllers.rs
+++ src/controllers.rs
@@ -2,8 +2,11 @@ use crate::controllers::books::get_routes as book_routes;
 use crate::controllers::mime::get_routes as mime_routes;
 use crate::controllers::opds::get_opds;
 use axum::{routing::get, Router};
+use http::Method;
 use serde::{Deserialize, Serialize};
 use sqlx::SqlitePool;
+use tower::ServiceBuilder;
+use tower_http::cors::{Any, CorsLayer};
 use tower_http::services::ServeDir;
 
 pub mod books;
@@ -11,6 +14,12 @@ pub mod mime;
 pub mod opds;
 
 pub fn get_routes(pool: SqlitePool) -> Router<()> {
+    let cors = CorsLayer::new()
+        // allow `GET` and `POST` when accessing the resource
+        .allow_methods([Method::GET, Method::POST, Method::PUT])
+        // allow requests from any origin
+        .allow_origin(Any);
+
     Router::new()
         .nest_service("/", ServeDir::new("dist"))
         .route("/opds", get(get_opds))
@@ -21,6 +30,7 @@ pub fn get_routes(pool: SqlitePool) -> Router<()> {
                 .nest("/mimes", mime_routes()),
         )
         .with_state(pool)
+        .layer(ServiceBuilder::new().layer(cors))
 }
 
 #[derive(Debug, Serialize, Clone)]
