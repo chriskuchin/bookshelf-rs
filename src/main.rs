9db1ce6d522cae1b3282faf49e5e0b667c9079b4
Chris Kuchin <chriskuchin@gmail.com>
2023-04-01 21:13:13 -0600

    handle server port for tablet env

--- src/main.rs
+++ src/main.rs
@@ -7,13 +7,18 @@ pub mod models;
 
 #[tokio::main]
 async fn main() {
+    let port: u16 = option_env!("NOMAD_PORT_run")
+        .unwrap_or("")
+        .parse::<u16>()
+        .unwrap_or(3000);
+
     let pool = SqlitePool::connect("sqlite://bookshelf.db?mode=rwc")
         .await
         .unwrap();
 
     sqlx::migrate!("./migrations").run(&pool).await.unwrap();
 
-    let addr = SocketAddr::from(([0, 0, 0, 0], 3000));
+    let addr = SocketAddr::from(([0, 0, 0, 0], port));
     println!("listening on {}", addr);
     axum::Server::bind(&addr)
         .serve(get_routes(pool).into_make_service())
