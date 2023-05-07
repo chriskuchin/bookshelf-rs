c67586a5df8d44222c56b5cb3eafdde79b40dfe2
Chris Kuchin <chriskuchin@gmail.com>
2023-05-07 12:15:22 -0600

    update

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 use crate::controllers::get_routes;
 use aws_sdk_s3::config::{Credentials, Region};
 use aws_sdk_s3::{Client, Config as StorageConfig};
-use config::Config;
+use clap::Parser;
 use serde::Deserialize;
 use sqlx::sqlite::SqlitePool;
 use std::net::SocketAddr;
@@ -9,16 +9,31 @@ use std::net::SocketAddr;
 pub mod controllers;
 pub mod models;
 
-#[derive(Debug, Deserialize, Clone)]
+#[derive(Debug, Deserialize, Clone, Parser)]
 #[allow(unused)]
 pub struct AppConfig {
+    #[arg(short, long, env = "BOOKSHELF_DB_URL")]
     db_url: String,
+
+    #[arg(short, long, env = "BOOKSHELF_STORAGE_URL")]
     storage_url: String,
-    port: u16,
+
+    #[arg(short, long, env = "BOOKSHELF_PORT")]
+    port: Option<u16>,
+
+    #[arg(long, env = "BOOKSHELF_AWS_ACCESS_KEY_ID")]
     aws_access_key_id: String,
+
+    #[arg(long, env = "BOOKSHELF_AWS_SECRET_ACCESS_KEY")]
     aws_secret_access_key: String,
+
+    #[arg(long, env = "BOOKSHELF_AWS_S3_REGION")]
     aws_s3_region: String,
+
+    #[arg(long, env = "BOOKSHELF_S3_ENDPOINT_URL")]
     aws_s3_endpoint_url: String,
+
+    #[arg(short, long, env = "BOOKSHELF_FRONTEND_LOCATION")]
     frontend_location: Option<String>,
 }
 
@@ -29,15 +44,7 @@ async fn main() {
         .parse::<u16>()
         .unwrap();
 
-    let settings = Config::builder()
-        .set_default("port", port)
-        .unwrap()
-        .add_source(config::File::with_name("config"))
-        .add_source(config::Environment::with_prefix("BOOKSHELF"))
-        .build()
-        .unwrap()
-        .try_deserialize::<AppConfig>()
-        .unwrap();
+    let settings = AppConfig::parse();
 
     let storage_creds = Credentials::new(
         &settings.aws_access_key_id,
@@ -59,7 +66,7 @@ async fn main() {
 
     sqlx::migrate!("./migrations").run(&pool).await.unwrap();
 
-    let addr = SocketAddr::from(([0, 0, 0, 0], settings.port));
+    let addr = SocketAddr::from(([0, 0, 0, 0], settings.port.unwrap_or(port)));
     println!("listening on {}", addr);
     axum::Server::bind(&addr)
         .serve(get_routes(pool, storage_client, settings).into_make_service())
