a4043ec5bf3ffdda89f06143cb43259dfef67227
Chris Kuchin <github@kchn.dev>
2023-09-15 21:53:55 -0600

    Add missing rust files

---
+++ src/controllers/books/series.rs
@@ -0,0 +1,32 @@
+use super::AppConfig;
+use aws_sdk_s3::Client;
+use axum::{
+    extract::State,
+    response::{IntoResponse, Response},
+    routing::get,
+    Router,
+};
+use http::StatusCode;
+use sqlx::SqlitePool;
+
+pub fn get_routes() -> Router<(SqlitePool, Client, AppConfig)> {
+    Router::new().route("/", get(list_series).post(create_series))
+}
+
+pub async fn list_series(
+    State((_pool, _storage, _settings)): State<(SqlitePool, Client, AppConfig)>,
+) -> Response {
+    (StatusCode::NOT_IMPLEMENTED).into_response()
+}
+
+pub async fn create_series(
+    State((_pool, _storage, _settings)): State<(SqlitePool, Client, AppConfig)>,
+) -> Response {
+    (StatusCode::NOT_IMPLEMENTED).into_response()
+}
+
+pub async fn update_series(
+    State((_pool, _storage, _settings)): State<(SqlitePool, Client, AppConfig)>,
+) -> Response {
+    (StatusCode::NOT_IMPLEMENTED).into_response()
+}
--- src/models/books/series.rs
+++ src/models/books/series.rs
@@ -1,1 +1,18 @@
+use futures::TryStreamExt;
+use sqlx::{Row, SqlitePool};
 
+const LIST_SERIES_QUERY: &str = "SELECT DISTINCT name FROM series ORDER BY name";
+pub async fn list_series(pool: &SqlitePool) -> Vec<String> {
+    let mut rows = sqlx::query(LIST_SERIES_QUERY).fetch(pool);
+
+    let mut authors: Vec<String> = Vec::new();
+
+    while let Some(row) = rows.try_next().await.unwrap() {
+        authors.push(row.try_get(0).unwrap());
+    }
+
+    return authors;
+}
+
+const _CREATE_SERIES_QUERY: &str = "INSERT into series (name) VALUES(?)";
+const _UPDATE_SERIES_QUERY: &str = "UPDATE series SET name = ? WHERE series_id = ?";
