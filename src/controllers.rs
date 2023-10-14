c77a9b6e0c11a4592d5df007c3eed82bccaf958f
Chris Kuchin <github@kchn.dev>
2023-10-13 21:14:38 -0600

    try fixing

--- src/controllers.rs
+++ src/controllers.rs
@@ -4,15 +4,10 @@ use crate::controllers::mime::get_routes as mime_routes;
 use crate::controllers::opds::get_opds;
 use aws_sdk_s3::Client;
 use axum::extract::DefaultBodyLimit;
-use axum::response::IntoResponse;
 use axum::{routing::get, Router};
-
-use http::Method;
-use http::StatusCode;
 
 use serde::Serialize;
 use sqlx::SqlitePool;
-use tower::ServiceBuilder;
 use tower_http::cors::{Any, CorsLayer};
 use tower_http::services::{ServeDir, ServeFile};
 use tower_http::trace::{self, TraceLayer};
@@ -24,12 +19,6 @@ pub mod opds;
 pub mod series;
 
 pub fn get_routes(pool: SqlitePool, storage_client: Client, settings: AppConfig) -> Router<()> {
-    let cors = CorsLayer::new()
-        // allow `GET` and `POST` when accessing the resource
-        .allow_methods([Method::GET, Method::POST, Method::PUT, Method::DELETE])
-        // allow requests from any origin
-        .allow_origin(Any);
-
     Router::new()
         .nest_service(
             "/",
@@ -41,11 +30,10 @@ pub fn get_routes(pool: SqlitePool, storage_client: Client, settings: AppConfig)
             "/api/v1",
             Router::new()
                 .nest("/books", book_routes())
-                .nest("/mimes", mime_routes())
-                .fallback(handler_404),
+                .nest("/mimes", mime_routes()),
         )
         .with_state((pool, storage_client, settings))
-        .layer(ServiceBuilder::new().layer(cors))
+        .layer(CorsLayer::new().allow_methods(Any).allow_origin(Any))
         .layer(DefaultBodyLimit::disable())
         .layer(
             TraceLayer::new_for_http()
@@ -54,9 +42,9 @@ pub fn get_routes(pool: SqlitePool, storage_client: Client, settings: AppConfig)
         )
 }
 
-async fn handler_404() -> impl IntoResponse {
-    (StatusCode::NOT_FOUND, "nothing to see here")
-}
+// async fn handler_404() -> impl IntoResponse {
+//     (StatusCode::NOT_FOUND, "nothing to see here")
+// }
 
 #[derive(Debug, Serialize, Clone)]
 pub struct Message {
