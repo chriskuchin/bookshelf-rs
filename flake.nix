de31c3aa491434f4e505566277c83a7444b6b4f1
Chris Kuchin <github@kchn.dev>
2024-02-02 21:55:54 -0700

    update

--- flake.nix
+++ flake.nix
@@ -17,34 +17,72 @@
 
   outputs = { self, nixpkgs, crane, flake-parts, flake-utils, ... }:
     flake-utils.lib.eachDefaultSystem (system:
-      let
+let
         pkgs = nixpkgs.legacyPackages.${system};
 
+        inherit (pkgs) lib;
+
         craneLib = crane.lib.${system};
-        my-crate = craneLib.buildPackage {
-          src = craneLib.cleanCargoSource (craneLib.path ./.);
+
+        sqlFilter = path: _type: null != builtins.match ".*sql$" path;
+        sqlOrCargo = path: type: (sqlFilter path type) || (craneLib.filterCargoSources path type);
+
+        src = lib.cleanSourceWith {
+          src = craneLib.path ./.; # The original, unfiltered source
+          filter = sqlOrCargo;
+        };
+
+        # Common arguments can be set here to avoid repeating them later
+        commonArgs = {
+          inherit src;
           strictDeps = true;
+
+          nativeBuildInputs = [
+            pkgs.pkg-config
+          ];
 
           buildInputs = [
             # Add additional build inputs here
-          ] ++ pkgs.lib.optionals pkgs.stdenv.isDarwin [
+            pkgs.openssl
+          ] ++ lib.optionals pkgs.stdenv.isDarwin [
             # Additional darwin specific inputs can be set here
             pkgs.libiconv
+            pkgs.darwin.apple_sdk.frameworks.Security
           ];
 
           # Additional environment variables can be set directly
           # MY_CUSTOM_VAR = "some value";
         };
+
+        # Build *just* the cargo dependencies, so we can reuse
+        # all of that work (e.g. via cachix) when running in CI
+        cargoArtifacts = craneLib.buildDepsOnly commonArgs;
+
+        # Build the actual crate itself, reusing the dependency
+        # artifacts from above.
+        my-crate = craneLib.buildPackage (commonArgs // {
+          inherit cargoArtifacts;
+
+          nativeBuildInputs = (commonArgs.nativeBuildInputs or [ ]) ++ [
+            pkgs.sqlx-cli
+          ];
+
+          preBuild = ''
+            export DATABASE_URL=sqlite:./db.sqlite3
+            sqlx database create
+            sqlx migrate run
+          '';
+        });
       in
       {
         checks = {
+          # Build the crate as part of `nix flake check` for convenience
           inherit my-crate;
         };
 
-        packages.default = my-crate;
-
-        apps.default = flake-utils.lib.mkApp {
-          drv = my-crate;
+        packages = {
+          default = my-crate;
+          inherit my-crate;
         };
 
         devShells.default = craneLib.devShell {
@@ -56,6 +94,7 @@
 
           # Extra inputs can be added here; cargo and rustc are provided by default.
           packages = [
+            pkgs.sqlx-cli
             pkgs.cargo-edit
             pkgs.just
             pkgs.sqlite
